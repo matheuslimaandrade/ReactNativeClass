03b33bc86f11bc3dfe69353514da57baa4000aa3
Matheus Lima Andrade <matheusla2f04@gmail.com>
2023-08-20 22:05:53 -0300

    firstScn first components

--- FirstExpoApp/App.js
+++ FirstExpoApp/App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden={true} />
+      <ImageBackground
+        source={require("./assets/Pokemon1.jpg")}
+        style={styles.image}
+      >
+        <View style={styles.box}>
+          <Text style={styles.tittle}>Welcome to PokeStats!</Text>
+          <Text style={styles.subTittle}>
+            Here you can see stats of all your Pokemons
+          </Text>
+        </View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            alert("PokeStats Login");
+          }}
+        >
+          <Text style={styles.tittle}>Login</Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +37,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: "#fff",
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "contain",
+  },
+  box: {
+    backgroundColor: "white",
+    borderRadius: 24,
+    padding: 14,
+    margin: 36,
+    marginTop: 36,
+  },
+  button: {
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    borderRadius: 12,
+    margin: 40,
+    height: 80,
+  },
+  tittle: {
+    color: "black",
+    fontSize: 28,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  subTittle: {
+    backgroundColor: "white",
+    padding: 12,
+    color: "red",
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
